08ee6064a2f55e6bfc4ecd4d93bb918591f53fe7
DevilsDomain <alaraerzincan@gmail.com>
2024-03-25 11:46:23 +0100

    add compression with shrink-ray

--- server/server.js
+++ server/server.js
@@ -2,19 +2,17 @@
 import express from 'express';
 import { fileURLToPath } from 'url';
 import path from 'path';
+import shrinkRay from 'shrink-ray-current'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = 3000;
+app.use(shrinkRay());
 
-// Serve static files from the 'dist' directory
 app.use(express.static(path.join(__dirname, '../dist')));
 
-// Define other routes or middleware as needed
-
-// Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
