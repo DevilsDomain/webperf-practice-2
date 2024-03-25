4621673d0dfbbe4b3503492c26d5368020d6980b
DevilsDomain <alaraerzincan@gmail.com>
2024-03-25 11:23:29 +0100

    express serving built FE site

--- server/server.js
+++ server/server.js
@@ -1,12 +1,20 @@
+// server/app.js
 import express from 'express';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, Express!');
-});
+// Serve static files from the 'dist' directory
+app.use(express.static(path.join(__dirname, '../dist')));
 
+// Define other routes or middleware as needed
+
+// Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
