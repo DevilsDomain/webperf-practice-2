07a2d778525b100c9fbe9910a463a026a69254da
DevilsDomain <alaraerzincan@gmail.com>
2024-03-27 16:32:23 +0100

    imported styles in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,8 @@
 import { AppProps } from 'next/app';
+import '../styles/bootstrap-icons.css';
+import '../styles/bootstrap.min.css';
+import '../styles/tooplate-crispy-kitchen.css'
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
